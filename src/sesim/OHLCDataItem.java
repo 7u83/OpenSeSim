fd40c3d03e36ba3b8c51d4d3d03199730646f27a
7u83 <7u83@mail.ru>
2025-10-05 14:12:03 +0200

    Constructor to create a OHLCDataItem from o,h,l,l
    Added some comments.

--- src/sesim/OHLCDataItem.java
+++ src/sesim/OHLCDataItem.java
@@ -37,14 +37,47 @@ public class OHLCDataItem {
     public float close;
     public float volume;
     public long time;
-    
-    
-    public OHLCDataItem(long time,float price, float volume){
-        this.time=time;
-        open=low=high=close=price;
-        this.volume=volume;
+
+    /**
+     * Constructor for creating a new bar/item where the price has just started
+     * (Open=High=Low=Close).
+     *
+     * @param time The timestamp for this data item in milliseconds
+     * @param price The initial price, used for all OHLC values.
+     * @param volume The initial trading volume.
+     */
+    public OHLCDataItem(long time, float price, float volume) {
+        // Calls the fully-parameterized constructor
+        this(time, price, price, price, price, volume);
     }
 
+    /**
+     * Fully-parameterized constructor to set all OHLC and volume values.
+     *
+     * @param time The timestamp for this data item in milliseconds
+     * @param open The opening price.
+     * @param high The highest price reached.
+     * @param low The lowest price reached.
+     * @param close The closing price.
+     * @param volume The total trading volume.
+     */
+    public OHLCDataItem(long time, float open, float high, float low, float close, float volume) {
+        this.time = time;
+        this.open = open;
+        this.high = high;
+        this.close = close;
+        this.low = low;
+        this.volume = volume;
+    }
+
+    /**
+     * Updates the current data item with a new price and volume tick. This
+     * adjusts high/low and updates the close price and total volume.
+     *
+     * @param price The latest price tick.
+     * @param volume The volume of the latest trade.
+     * @return true if the high or low price was updated, false otherwise.
+     */
     public boolean update(float price, float volume) {
         boolean ret = false;
         if (price > high) {
@@ -60,10 +93,14 @@ public class OHLCDataItem {
         this.close = price;
         return ret;
     }
-    
-    public float getAverage(){
-        return (open+high+low+close)/4;
+
+    /**
+     * Calculates the average of the open, high, low, and close prices.
+     *
+     * @return The simple average price for the period.
+     */
+    public float getAverage() {
+        return (open + high + low + close) / 4;
     }
-    
 
 }
