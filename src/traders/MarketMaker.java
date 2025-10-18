ba496c5d1e9ef3ef5f33b4995e3124975d20152e
7u83 <7u83@mail.ru>
2025-10-18 15:18:19 +0200

    Start of Marke maker implementation

--- src/traders/MarketMaker.java
+++ src/traders/MarketMaker.java
@@ -7,16 +7,33 @@ package traders;
 import org.json.JSONObject;
 import sesim.AutoTraderBase;
 import sesim.AutoTraderGui;
+import sesim.Exchange.Order;
 import sesim.Scheduler;
+
 
 /**
  *
  * @author tube
  */
 public class MarketMaker extends AutoTraderBase {
+    
+    int numPosition=10;
+    float centerPrice=100.0f;
+    float lowestPrice=0.0f;
+    
+    Order orders[]=null;
 
     @Override
     public void start() {
+        float dist=(centerPrice-lowestPrice)/this.numPosition;
+        
+        centerPrice = se.getBestPrice();
+        
+       /* while(false){
+        
+        }*/
+        
+        this.setStatus("Price: %f",p);
 
     }
 
