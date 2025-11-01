8e87d21ac431fd82697565b23dc47d97c662f9e1
7u83 <7u83@mail.ru>
2025-11-01 19:53:10 +0100

    Updated default.groovy

--- src/main/resources/files/GroovyTrader/default.groovy
+++ src/main/resources/files/GroovyTrader/default.groovy
@@ -12,8 +12,8 @@
 
 import groovy.transform.Field
 
-@Field INITIAL_DELAY_MIN = 60000
-@Field INITIAL_DELAY_MAX = 240000
+@Field INITIAL_DELAY_MIN = 60000*1
+@Field INITIAL_DELAY_MAX = 60000*5
 
 
 def start(){
@@ -30,42 +30,74 @@ def myEvent;
 def myOrder;
 
 def runTrader(){
-    // buy 100000 shares (or as many as cash is available)
-    sesim.createOrder(sesim.BUY,100000,0.0d,0.0d);
-    // enable account update notifications
-    sesim.onAccountUpdate("accountUpdated");
+    	// buy 100000 shares (or as many as cash is available)
+    	sesim.setStatus("%s: Waiting for market order to be filled",sesim.getName());
+    	sesim.onAccountUpdate("marketOderFilled");
+    	sesim.createOrder(sesim.BUY,100000,0.0d,0.0d);
 
-    // calculate stop loss and take profit
-    lastPrice = sesim.getLastPrice();
-    takeProfitPrice = lastPrice+lastPrice/100*10;
-    stopLoss = lastPrice-lastPrice/100*10;
 
-    // create a stop loss order
-    myOrder = sesim.createOrder(sesim.STOPLOSS,account.getShares(),0,stopLoss);
 
-    // register a notificate when price hits take profit price
-    myEvent = sesim.scheduleOnPriceAbove("takeProfit",takeProfitPrice);
+}
 
-	
+def marketOderFilled(def o){
+	if (!o.isClosed()){
+		return;
+	}
+
+    	// calculate stop loss and take profit	
+    	lastPrice = o.getAveragePrice();
+
+	//sesim.logInfo("%s: Groovy LastPrice: %f",sesim.getName(),lastPrice);
+    	
+    	takeProfitPrice = lastPrice+lastPrice/100*10;
+    	stopLoss = lastPrice-lastPrice/100*10;
+
+    	sesim.setStatus("Waiting for tp:%.2f or sl: %.2f",takeProfitPrice,stopLoss);	
+
+   //	sesim.logInfo("%s: Waiting for tp:%.2f or sl: %.2f",sesim.getName(),takeProfitPrice,stopLoss);
+    	
+ 	// enable account update notifications
+    	sesim.onAccountUpdate("accountUpdated");
+
+    	// create a stop loss order
+    	myOrder = sesim.createOrder(sesim.STOPLOSS,account.getShares(),0,stopLoss);
+
+    	// register a notificate when price hits take profit price
+	myEvent = sesim.scheduleOnPriceAbove("takeProfit",takeProfitPrice);
+
+
 }
 
 def takeProfit(){
-    // take profit price hit, cancel the stop loss order
-    sesim.cancleOrder(myOrder);
-    // sell all shares
-    sesim.createOrder(sesim.SELL,account.getShares(),0,0);
-    // start over
+	//sesim.logInfo("%s: Take profit price reached", sesim.getName());
+	sesim.onAccountUpdate(null);
+    	// take profit price hit, cancel the stop loss order
+    	sesim.cancleOrder(myOrder);
+
+	sesim.onAccountUpdate("allSold");
+    	// sell all shares
+    	sesim.createOrder(sesim.SELL,account.getShares(),0,0);
+
+}
+
+def allSold(def o){
+	if (!o.isClosed()){
+		return;
+	}
+	    // start over
+//	    sesim.logInfo("%s: All sold, start over", sesim.getName());
     start();
 }
 
 def accountUpdated(def o){
-    if (o.isClosed() && o.hasStop() ){
-        // our stop loss order was executed
-        // cancel the take profit event
-        sesim.cancelSchedulePriceAboce(myEvent);
+	if (o.isClosed() && o.hasStop() ){
+	//	sesim.logInfo("%s: Stoploss triggered",sesim.getName());
+		// our stop loss order was executed
+		// cancel the take profit event
+		sesim.cancelScheduleOnPriceAbove(myEvent);
 
-        // start over
-        start();
+        	// start over
+        	start();
     }
 	
 }
